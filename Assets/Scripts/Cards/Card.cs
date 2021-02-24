e41d6d7a6e0a6604cc51e478c501302a0c794090
Adam Furmanek <adam.furmanek.adios@gmail.com>
2021-02-24 23:15:45 +0100

    Mechanika walki 1

---
+++ Assets/Scripts/Cards/Card.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Card : MonoBehaviour
+{
+    public float oAttack, oDefense, oHealth;
+    public float attack, defense, health;
+    public int oUses, oSleeps;
+    public int uses, sleeps;
+
+    public Match match;
+    public int status;
+    public int player;
+
+    public void PrepareForBattle(Match match, int player)
+    {
+        this.match = match;
+        ResetStats();
+        this.player = player;
+        status = 0;
+        match.cards[player, status].Add(this);
+    }
+
+    public void ResetStats()
+    {
+        attack = oAttack;
+        defense = oDefense;
+        health = oHealth;
+        uses = oUses;
+        sleeps = oSleeps;
+    }
+
+    public void OnMouseDown()
+    {
+        if (match.selectedCard == null)
+        {
+            switch (status)
+            {
+                case 0:
+                    DrawACard();
+                    break;
+                case 1:
+                    PutOnTable();
+                    break;
+                case 2:
+                    match.selectedCard = this;
+                    break;
+            }
+        }
+        else
+            if (match.selectedCard != this)
+                match.ExecuteOnSecondCard(this);
+
+        match.CheckDeaths();
+        match.CleanTable();
+    }
+
+    public virtual void DrawACard()
+    {
+        match.cards[player, status].Remove(this);
+        status = 1;
+        match.cards[player, status].Add(this);
+    }
+
+    public virtual void PutOnTable()
+    {
+        match.cards[player, status].Remove(this);
+        status = 2;
+        match.cards[player, status].Add(this);
+    }
+
+    public virtual void UseOnSomething(Card other)
+    {
+        health = 0;
+        other.health = 0;
+    }
+
+    public virtual void Death()
+    {
+        match.cards[player, status].Remove(this);
+        status = 3;
+        match.cards[player, status].Add(this);
+    }
+
+    public virtual void Resurrection(int status)
+    {
+
+    }
+
+    public virtual void OnNewTurn()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Match.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Match : MonoBehaviour
+{
+    public List<Card>[,] cards =
+    {
+        {new List<Card>(), new List<Card>(), new List<Card>(), new List<Card>()},
+        {new List<Card>(), new List<Card>(), new List<Card>(), new List<Card>()}
+    };
+
+    public int currentPlayer;
+    public Card selectedCard = null;
+
+    public int OpposingPlayer()
+    {
+        return currentPlayer == 0 ? 1 : 0;
+    }
+
+    public void ExecuteOnSecondCard(Card other)
+    {
+        Debug.Log("ok");
+        selectedCard.UseOnSomething(other);
+        selectedCard = null;
+    }
+
+    public void Start()
+    {
+        List<string> playerCards = new List<string>();
+        List<string> enemyCards = new List<string>();
+        for(int i= 0; i < 10; i++)
+        {
+            playerCards.Add("Soldier");
+            enemyCards.Add("Soldier");
+        }
+
+        StartMatch(playerCards, enemyCards);
+        CleanTable();
+
+    }
+
+    public void StartMatch(List<string> player0Cards, List<string> player1Cards)
+    {
+        List<string>[] playersCards = { player0Cards, player1Cards };
+        for(int i = 0; i < playersCards.Length; i++)
+        {
+            for (int j = 0; j < playersCards[i].Count; j++)
+            {
+                GameObject go = (GameObject)Resources.Load("prefabs/"+ playersCards[i][j], typeof(GameObject));
+                Card card = Instantiate(go, new Vector3(0, 0, 0), go.transform.rotation).GetComponent<Card>();
+                card.PrepareForBattle(this, i);
+            }
+        }
+    }
+
+    public void CheckDeaths()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = cards[i,2].Count - 1; j >= 0; j--)
+            {
+                if (cards[i, 2][j].health <= 0)
+                    cards[i, 2][j].Death();
+            }
+        }
+    }
+
+    public void CleanTable()
+    {
+        for (int i = 0; i < cards[0, 0].Count; i++)
+            cards[0, 0][i].gameObject.transform.position = new Vector3(8.0f, (float)i / 100, 2.0f);
+        for (int i = 0; i < cards[1, 0].Count; i++)
+            cards[1, 0][i].gameObject.transform.position = new Vector3(8.0f, (float)i / 100, -2.0f);
+
+        for (int i = 0; i < cards[0, 1].Count; i++)
+            cards[0, 1][i].gameObject.transform.position = new Vector3(-8.0f + 1.8f * i, 0, 3.3f);
+        for (int i = 0; i < cards[1, 1].Count; i++)
+            cards[1, 1][i].gameObject.transform.position = new Vector3(-8.0f + 1.8f * i, 0, -3.3f);
+
+        for (int i = 0; i < cards[0, 2].Count; i++)
+            cards[0, 2][i].gameObject.transform.position = new Vector3(-8.0f + 1.8f * i, 0, 1.2f);
+        for (int i = 0; i < cards[1, 2].Count; i++)
+            cards[1, 2][i].gameObject.transform.position = new Vector3(-8.0f + 1.8f * i, 0, -1.2f);
+
+        for (int i = 0; i < cards[0, 3].Count; i++)
+            cards[0, 3][i].gameObject.transform.position = new Vector3(50.0f, 0, 0);
+        for (int i = 0; i < cards[1, 3].Count; i++)
+            cards[1, 3][i].gameObject.transform.position = new Vector3(50.0f, 0, 0);
+    }
+
+    public void OnMouseDown()
+    {
+        selectedCard = null;
+    }
+
+    public void GiveTurn()
+    {
+        currentPlayer = OpposingPlayer();
+        for (int i = 0; i < cards[2,currentPlayer].Count; i++)
+            cards[2,currentPlayer][i].OnNewTurn();
+
+        CheckDeaths(); //Sprawdź czy ktoś umarł/przegrał.
+    }
+
+}
